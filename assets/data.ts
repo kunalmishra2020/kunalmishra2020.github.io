cbea198d4dafa69535b197b033c0dc65e5349fe9
Kunal Mishra <kunalmishradev@gmail.com>
2023-03-08 17:02:30 +0530

    Auto-generated commit [ci skip]

---
+++ assets/data.ts
@@ -0,0 +1,45 @@
+export const user = {
+	name: 'Kunal Mishra',
+	summary: `
+As a highly skilled and motivated software engineer, I bring 3+ years of experience in developing and deploying scalable applications on the cloud. I am proficient in Node.js and have expertise in AWS services, including Lambda, API Gateway, and Postgres. With an in-depth understanding of serverless architecture, I have developed and maintained serverless applications that offer high-performance, fault-tolerant and cost-effective solutions.
+
+My skills in Git enable me to manage version control efficiently, ensuring that the codebase is always up to date and easily accessible to the development team. Furthermore, I possess excellent problem-solving skills, allowing me to identify and troubleshoot issues quickly, minimizing downtime and ensuring the smooth operation of applications.
+
+I am passionate about keeping up with the latest industry trends and standards, which enables me to deliver high-quality work that meets or exceeds expectations. As a team player, I thrive in collaborative environments and enjoy contributing to project success through effective communication, feedback, and knowledge sharing.
+	`,
+	projects: [
+		{
+			name: '',
+			startDate: new Date(),
+			endDate: <const>'present',
+			shortDescription: '',
+			keyPoints: [''],
+		},
+	],
+	contributions: [
+		{
+			title: 'string',
+			url: 'string',
+			description: 'string',
+		},
+	],
+	skills: ['NodeJs', 'Amazon Web Services (AWS)', 'TypeScript', 'JavaScript', 'Python', 'MongoDB', 'PostgreSQL', 'Git'],
+	blogPosts: [
+		{
+			title: 'd',
+			summary: 'd',
+			url: 'd',
+		},
+	],
+	contacts: [
+		{
+			url: 'https://github.com/kunalmishra2020',
+			type: 'github' as const,
+		},
+		{
+			url: 'https://www.linkedin.com/in/kunalmishra2020',
+			type: 'linkedIn' as const,
+		},
+	],
+	resume: 'assets/KunalMishra.pdf',
+};
